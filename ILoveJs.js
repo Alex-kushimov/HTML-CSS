ad30b34e253099e29311c10a0b84c5b0ff393e76
AlexKushimov <alexkushimov@gmail.com>
2016-03-09 12:10:44 +0200

    first

--- ILoveJs.js
+++
@@ -1,9 +0,0 @@
-function reverseStr (str) {
-	var result = "", i;
-	for (i = str.length - 1; i >= 0; i--) {
-	result += str.charAt(i)
-	};
-  return result;
-
-}
-alert(reverseStr("I love javascript"));
--- homework.js
+++ homework.js
@@ -1,26 +1,9 @@
-var a = 10;
-var	b = 30;
-var c = 20;
-if (a>b && a>c) {
- a*=10;
- if(b>c){
-  b*=5
- } else {
-  c*=5
- }
-} else if (b>a && b>c) {
- b*=10;
- if(a>c){
-  a*=5;
- } else {
-  c*=5;
- }
-} else if (c>a && c>b) {
- c*=10;
- if (a>b) {
-  a*=5;
- } else {
-  b*=5;
- }
-};
-console.log(a,b,c);
+function reverseStr (str) {
+	var result = "", i;
+	for (i = str.length - 1; i >= 0; i--) {
+	result += str.charAt(i)
+	};
+  return result;
+
+}
+alert(reverseStr("I love javascript"));
